bbde5db4774148af91784a230cce3c432ee03dc5
Highfivethefive <jadejwolfe@gmail.com>
2025-02-10 19:41:20 -0700

    added word count function and the ability to change and read books easier

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+#main function to print out a book script
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    book_path = "books/frankenstein.txt"
+    file_contents = get_book_text(book_path)
 
-        print(file_contents)
+
+
+    count = bookwordcount(file_contents)
+    print(f"the book contains {count} words.")
+        
+
+# read each word in the book and return a number
+def bookwordcount(book_content):
+    print("counting the word count for the book...")
+    words = book_content.split()
+    return len(words)
+
+# read the contents of a file and return it as a string
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 
